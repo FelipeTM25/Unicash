daf212f5299960e29dfd85fd9488cf87315840c9
FelipeTM25 <felipe122505@gmail.com>
2026-03-14 11:06:46 -0500

    cambio paquete

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.unicash.app',
+  appId: 'com.upb.unicash.estudiantes',
   appName: 'unicash',
   webDir: 'dist'
 };
