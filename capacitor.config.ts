f189aa8f720d64b86d65762ba687c560542029da
FelipeTM25 <felipe122505@gmail.com>
2026-03-17 07:11:07 -0500

    Cambio logo android studio y capitalización V3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.upb.unicash.estudiantes',
-  appName: 'unicash',
+  appName: 'Unicash',
   webDir: 'dist'
 };
 
